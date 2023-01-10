57528c2c1e29981c30530d6f432a6a3c33f6e3d9
therealmolf <miksbon@gmail.com>
2023-01-10 11:51:31 +0800

    changed runtime.txt to 3.9.5

--- src/utils/db_connect.py
+++ src/utils/db_connect.py
@@ -2,23 +2,21 @@ import psycopg2
 import pandas as pd
 import os
 
-
-HOST = 'ec2-3-209-124-113.compute-1.amazonaws.com'
-DATABASE = 'd53gumo0i7ad3h'
-USER = 'ethiewwcdjsojh'
-PORT = 5432
-PASSWORD = '0c09d8a125a30a74980999d9d33943f7ac3d845d57684327c7378bc53b924ad0'
+LOCAL_HOST = "localhost"
+LOCAL_DB = "mooki_db"
+LOCAL_USER = "postgres"
+LOCAL_PASSWORD = "rickyrubio17"
 
 
 def get_db_conn():
-    # DATABASE_URL = os.environ['DATABASE_URL']
+    DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(
-        host=HOST,
-        database=DATABASE,
-        user=USER,
-        port=PORT,
-        password=PASSWORD
-        # DATABASE_URL,
+        # host=LOCAL_HOST,
+        # database=LOCAL_DB,
+        # user=LOCAL_USER,
+        # port=PORT,
+        # password=LOCAL_PASSWORD
+        DATABASE_URL,
         # sslmode='require'
     )
     return conn
