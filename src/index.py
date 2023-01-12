d6a1044a9d3e6c967be3f432b4d3028574a747fb
therealmolf <miksbon@gmail.com>
2023-01-13 01:18:39 +0800

    removed init in src and added .app

--- src/index.py
+++ src/index.py
@@ -3,7 +3,7 @@ from dash import html, dcc
 from dash.dependencies import Input, Output
 
 # Connect to main app.py file
-from app import app
+from .app import app
 
 # Connect to your app pages
 from pages import home, about_page, proposal_page, approve_page
